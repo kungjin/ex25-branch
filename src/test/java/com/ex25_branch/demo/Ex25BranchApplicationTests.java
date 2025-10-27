b3a183dc6d290745a2227eaae34fdc8f55668873
kungjin <vudchsdl123@gmail.com>
2025-10-27 11:28:35 +0900

    Initial commit

---
+++ src/test/java/com/ex25_branch/demo/Ex25BranchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex25_branch.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex25BranchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
